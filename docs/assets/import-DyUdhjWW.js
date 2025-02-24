1a35799cd36eaec79914d90ce860e2eab4f3c0de
Andy Wardley <abw@wardley.org>
2025-02-24 13:13:34 +0000

    Docs build

---
+++ docs/assets/import-DyUdhjWW.js
@@ -0,0 +1,2 @@
+const o=`import { Model } from '@abw/react-model'
+`;export{o as default};
---
+++ docs/assets/install-xpGdbWdy.js
@@ -0,0 +1,9 @@
+const n=`# using npm
+$ npm add @abw/react-model
+
+# using yarn
+$ yarn add @abw/react-model
+
+# using pnpm
+$ pnpm add @abw/react-model
+`;export{n as default};
